643d602f8f4da20d29c0cd543bd04b15d5d5854e
Nick McDaniel <nickamcd@gmail.com>
2021-05-20 14:53:55 -0700

    Can now display books in library

--- script.js
+++ script.js
@@ -11,10 +11,6 @@ function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
-function displayLibrary() {
-
-}
-
 let newBookBtn = document.querySelector('#new-book')
 let newBookForm = document.querySelector('.form-container')
 
@@ -38,6 +34,21 @@ addForm.addEventListener('submit', function(e) {
 
   book = new Book(bookTitle, bookAuthor, bookPages, bookRead)
   addBookToLibrary(book)
+  newBookForm.style.display = 'none'
 
+  displayLibrary()
   console.log(book)
 })
+
+tableBody = document.querySelector('.table-body') 
+
+function displayLibrary() {
+  tableBody.innerHTML = myLibrary.map((book) => {
+    return `<tr>
+              <td>${book.title}</td>
+              <td>${book.author}</td>
+              <td>${book.pages}</td>
+              <td>${book.read}</td>
+            </tr>`
+  }).join('')
+}
